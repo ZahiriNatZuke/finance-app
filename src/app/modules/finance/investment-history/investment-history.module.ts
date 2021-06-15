6c00f41161caf8798ce3626b25f2bef5e61041ce
Yohan González <yohan.gonzalez.almaguer@gmail.com>
2021-06-15 16:00:25 -0400

    Investment History UI

--- src/app/modules/finance/investment-history/investment-history.module.ts
+++ src/app/modules/finance/investment-history/investment-history.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { InvestmentHistoryRoutingModule } from './investment-history-routing.module';
 import { InvestmentHistoryComponent } from './investment-history.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
 
 
 @NgModule({
@@ -11,7 +15,11 @@ import { InvestmentHistoryComponent } from './investment-history.component';
   ],
   imports: [
     CommonModule,
-    InvestmentHistoryRoutingModule
+    InvestmentHistoryRoutingModule,
+    FlexLayoutModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatSelectModule
   ]
 })
 export class InvestmentHistoryModule { }
