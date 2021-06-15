fc8708d8c61cabc8861d44c4818530802e14f9f7
Yohan González <yohan.gonzalez.almaguer@gmail.com>
2021-06-15 17:43:19 -0400

    Set i18n in Global Position module

--- src/app/modules/finance/global-position/global-position.module.ts
+++ src/app/modules/finance/global-position/global-position.module.ts
@@ -9,21 +9,23 @@ import {MatExpansionModule} from '@angular/material/expansion';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 import {MatOptionModule} from '@angular/material/core';
+import {TranslateModule} from '@ngx-translate/core';
 
 
 @NgModule({
   declarations: [
     GlobalPositionComponent
   ],
-  imports: [
-    CommonModule,
-    GlobalPositionRoutingModule,
-    FlexLayoutModule,
-    MatIconModule,
-    MatExpansionModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatOptionModule
-  ]
+    imports: [
+        CommonModule,
+        GlobalPositionRoutingModule,
+        FlexLayoutModule,
+        MatIconModule,
+        MatExpansionModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatOptionModule,
+        TranslateModule
+    ]
 })
 export class GlobalPositionModule { }
