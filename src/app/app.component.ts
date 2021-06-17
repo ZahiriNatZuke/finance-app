1d9e86e2962ead1b9055ec25e91f1ec153d82cf2
Yohan González <yohan.gonzalez.almaguer@gmail.com>
2021-06-17 09:59:15 -0400

    Added missing configuration of i18n service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {environment} from '../environments/environment';
 import {Logger} from './modules/shared/utils/logger';
 import {I18nService} from './modules/shared/modules/i18n/i18n.service';
@@ -8,15 +8,17 @@ import {I18nService} from './modules/shared/modules/i18n/i18n.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 
   constructor(private i18nService: I18nService) {
     this.i18nService.init(environment.defaultLanguage, environment.supportedLanguages);
   }
 
   ngOnInit(): void {
-    if (environment.production) {
-      Logger.enableProductionMode();
-    }
+    if (environment.production) Logger.enableProductionMode();
+  }
+
+  ngOnDestroy(): void {
+    this.i18nService.destroy();
   }
 }
